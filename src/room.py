9867793ba9bade36fa84f9cd41e7e071e3e3bd4d
agohorel <3317203+agohorel@users.noreply.github.com>
2020-05-07 19:03:11 -0400

    fix improper default assignment of room items

--- src/room.py
+++ src/room.py
@@ -2,10 +2,13 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description, items=[]):
+    def __init__(self, name, description, items=None):
         self.name = name
         self.description = description
-        self.items = items
+        if items == None:
+            self.items = []
+        else:
+            self.items = items
 
     def __str__(self):
         return f"current room: {self.name}, description: {self.description}, items: {self.items}"
